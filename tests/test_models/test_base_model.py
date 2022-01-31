dc3a40017c8ffa0bc40becddda1ffe918a508c17
Beldine-Moturi <beldinemoturi@gmail.com>
2022-01-31 22:28:16 +0300

    tests for created_at, updated_at attributes and save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Contains test cases for the BaseModel class used
 for all other classes in this project"""
-import unittest
+from datetime import datetime
 from models.base_model import BaseModel
+import unittest
+
 
 class TestBaseModelClass(unittest.TestCase):
     """Tests the Instance attributes and methods
@@ -24,14 +26,40 @@ class TestBaseModelClass(unittest.TestCase):
         """Tests that objects of this class are properly instantiated
         with a universally unique identifier"""
 
-        #test that the objects are assigned an id attribute during instantiation
         self.assertTrue(hasattr(self.model1, "id"))
-
-        #tests that the id attribute is a string
         self.assertIs(type(self.model1.id), str)
-
-        #tests that the id attribute is a universally unique identifier
         self.assertNotEqual(self.model1.id, self.model2.id)
+
+    def test_created_at_attribute(self):
+        """Tests that the current datetime is assigned when
+        objects of this class are instantiated"""
+
+        t = datetime.now()
+        created = self.model1.created_at.isoformat(timespec='seconds')
+        self.assertTrue(hasattr(self.model1, "created_at"))
+        self.assertIs(type(self.model1.created_at), datetime)
+        self.assertGreaterEqual(t.isoformat(timespec='seconds'), created)
+
+    def test_save_method(self):
+        """Tests that this methods correctly updates the public instance
+        attribute updated_at with the current datetime when called"""
+
+        old = self.model1.updated_at
+        self.model1.save()
+        new = self.model1.updated_at
+        self.assertNotEqual(old, new)
+
+    def test_updated_at_attribute(self):
+        """Tests that the current datetime is assigned when
+        objects of this class are updated"""
+
+        self.assertTrue(hasattr(self.model1, "updated_at"))
+        self.assertIs(type(self.model1.updated_at), datetime)
+        self.model1.save()
+        t = datetime.now()
+        update = self.model1.updated_at.isoformat(timespec='seconds')
+        self.assertEqual(t.isoformat(timespec='seconds'), update)
+
 
 if __name__ == "__main__":
     unittest.main()
