d39a80c3883af7166ff6b8179b96ef119f5ea575
Goodness <atundeg@gmail.com>
2022-01-31 17:09:18 +0100

    added base_model.py file for task 3: BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+The Base Model module
+Defines all common attributes/methods for other classes
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """
+    The "BaseModel" class
+    Defines all common attributes/methods for other model classes
+    """
+    def __init__(self) -> None:
+        """Initializes a BaseModel object"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self) -> str:
+        """String representation of BaseModel object"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """'save' method
+        Updates the public instance attribute 'updated_at'\
+             with current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values\
+             of __dict__ of the instance"""
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+
+        new_dict = {}
+
+        for k, v in self.__dict__.items():
+            if k not in new_dict:
+                new_dict.update({k: v})
+        if '__class__' not in self.__dict__.items():
+            new_dict.update({'__class__': self.__class__.__name__})
+
+        return new_dict
