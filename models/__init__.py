258def74b1aac9996efb4c147258d89ffac122a6
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-06 03:25:17 +0300

    remove innit file

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Reloads all the objects created from precious sessions"""
-from .engine.file_storage import FileStorage
-
-
-__all__ = ['base_model', 'state', 'user', 'place', 'amenity', 'review', 'city']
-storage = FileStorage()
-storage.reload()
