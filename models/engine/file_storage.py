14dc5c782091b4ba7f24b1d25b4429adb14dbff8
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-06 03:58:49 +0300

    pep8 validation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 """Defines a class FileStorage that
 serializes instances to a JSON file and deserializes JSON file to instances"""
 import json
-#from models.base_model import BaseModel
 
 
 class FileStorage():
