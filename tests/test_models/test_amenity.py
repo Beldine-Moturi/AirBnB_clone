311217dcccfe0558fdd04d882f289c191c42bd5d
Beldine-Moturi <beldinemoturi@gmail.com>
2022-02-07 00:06:43 +0300

    write more tests for amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Contains tests for the Class User"""
-import json
-from models import storage
+from contextlib import redirect_stdout
+from datetime import datetime
+from models.base_model import BaseModel
+import io
 import os
 from models.amenity import Amenity
 import unittest
@@ -11,47 +13,91 @@ class TestAmenityClassAttributes(unittest.TestCase):
     """Tests instantiation of Amenity objects and all the attributes
     defined in the class"""
 
-    def test_amenity_attributes(self):
-        """tests the instantiations and dynamic allocation of attributes
-        to objects of the class Amenity"""
+    def setUp(self):
+        """sets up the resources needed to run the tests"""
 
-        amenity1 = Amenity()
-        amenity1.name = "WiFi"
+        self.amenity1 = Amenity()
 
-        self.assertTrue(hasattr(amenity1, "name"))
+    def tearDown(self):
+        """Deletes the resources created when running the tests"""
+
+        del self.amenity1
+
+    def test_issubclass(self):
+        """Tests if Amenity is a subclass of BaseModel"""
+
+        self.assertIsInstance(self.amenity1, BaseModel)
+        self.assertTrue(hasattr(self.amenity1, "id"))
+        self.assertTrue(hasattr(self.amenity1, "created_at"))
+        self.assertTrue(hasattr(self.amenity1, "updated_at"))
+
+    def test_name_attribute(self):
+        """Tests that objects of the class Amenity have the name attribute"""
+
+        self.assertTrue(hasattr(self.amenity1, "name"))
+        self.assertEqual(self.amenity1.name, "")
+        self.amenity1.name = "WiFi"
+        self.assertEqual(self.amenity1.name, "WiFi")
 
 
-class TestAmenityClassStorage(unittest.TestCase):
-    """Tests that objects of class Amenity are properly stored in the file
-    storage"""
+class TestAmenityClassMethods(unittest.TestCase):
+    """Tests that methods defined in Amenity's parent class work on instances
+    of class"""
 
-    @classmethod
-    def tearDownClass(cls):
-        """Tears down resources created when running the tests"""
+    def setUp(self):
+        """Sets up the resources needed to run the tests"""
 
-        os.remove("filestorage.json")
+        self.amenity2 = Amenity()
 
-    def test_file_storage(self):
-        """Tests the file storage with City objects"""
+    def tearDown(self):
+        """Deletes the resources created when running the tests"""
 
-        all_objs = storage.all()
-        self.assertEqual(len(all_objs), 1)
+        if (os.path.isfile("filestorage.json")):
+            os.remove("filestorage.json")
+        del self.amenity2
 
-        new_amenity1 = Amenity()
-        all_objs = storage.all()
-        self.assertEqual(len(all_objs), 2)
-        for key, value in all_objs.items():
-            self.assertTrue(type(value) == Amenity)
+    def test_save_method(self):
+        """Tests that this methods correctly updates the public instance
+        attribute updated_at with the current datetime when called"""
 
-        new_amenity1.save()
-        new_amenity2 = Amenity()
-        new_amenity2.save()
-        self.assertTrue(os.path.isfile("filestorage.json"))
-        storage.reload()
-        all_objs = storage.all()
-        self.assertTrue(len(all_objs) == 3)
-        for key, value in all_objs.items():
-            self.assertTrue(type(value) == Amenity)
+        old = self.amenity2.updated_at
+        self.amenity2.save()
+        new = self.amenity2.updated_at
+        self.assertNotEqual(old, new)
+
+    @staticmethod
+    def captured_output(obj):
+        """Captures what is printed to the standard output
+        when the print method is called on obj"""
+
+        captured_output = io.StringIO()
+        with redirect_stdout(captured_output):
+            print(obj)
+            return (captured_output.getvalue())
+
+    def test_str_method(self):
+        """Tests that the __str__ method correctly returns
+        a string representation of objects of this class"""
+
+        self.assertEqual(TestAmenityClassMethods.captured_output(
+            self.amenity2), f"[Amenity]\
+ ({self.amenity2.id}) {self.amenity2.__dict__}\n")
+
+    def test_to_dcit_method(self):
+        """Tests the to_dict method on Amenity objects"""
+
+        amenity_dict = self.amenity2.to_dict()
+
+        for att in ["id", "created_at", "updated_at","__class__"]:
+            self.assertIn(att, amenity_dict.keys())
+            self.assertTrue(type(amenity_dict[att]) == str)
+        self.assertEqual(amenity_dict["__class__"], "Amenity")
+
+        created_date = datetime.fromisoformat(amenity_dict["created_at"])
+        self.assertEqual(created_date, self.amenity2.created_at)
+
+        updated_at_date = datetime.fromisoformat(amenity_dict["updated_at"])
+        self.assertEqual(updated_at_date, self.amenity2.updated_at)
 
 
 if __name__ == "__main__":
